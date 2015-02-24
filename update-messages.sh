fa0119e4b1d8cde2e13651aca41c22ab9715c923
Michaël Lhomme <papylhomme@gmail.com>
2015-02-24 22:27:41 +0000

    CMake install language files, split app and applet dictionnary

--- update-messages.sh
+++ update-messages.sh
@@ -1,48 +1,67 @@
 #!/bin/sh
-BASEDIR="."  # root of translatable sources
-PROJECT="DisKMonitor"  # project name
+BASEDIR=`pwd`  # root of translatable sources
+PROJECT="diskmonitor"  # project name
+APPLET="plasma_applet_org.papylhomme.diskmonitor"  # project name
 BUGADDR="https://github.com/papylhomme/diskmonitor/issues"  # MSGID-Bugs
-WDIR=`pwd`    # working dir
+WDIR="${BASEDIR}/translations"    # working dir
  
  
 echo "Preparing rc files"
-cd ${BASEDIR}
 # we use simple sorting to make sure the lines do not jump around too much from system to system
-find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort > ${WDIR}/rcfiles.list
-xargs --arg-file=${WDIR}/rcfiles.list extractrc > ${WDIR}/rc.cpp
+find ./app -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort > ${WDIR}/app/rcfiles.list
+find ./settings -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort >> ${WDIR}/app/rcfiles.list
+xargs --arg-file=${WDIR}/app/rcfiles.list extractrc > ${WDIR}/app/rc.cpp
+
 # additional string for KAboutData
-echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> ${WDIR}/rc.cpp
-echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> ${WDIR}/rc.cpp
-cd ${WDIR}
+echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> ${WDIR}/app/rc.cpp
+echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> ${WDIR}/app/rc.cpp
 echo "Done preparing rc files"
  
  
 echo "Extracting messages"
-cd ${BASEDIR}
 # see above on sorting
-find . -name '*.cpp' -o -name '*.h' -o -name '*.c' -o -name '*.qml' | sort > ${WDIR}/infiles.list
-echo "rc.cpp" >> ${WDIR}/infiles.list
+
+#app
+find ./app -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort > ${WDIR}/app/infiles.list
+find ./settings -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort >> ${WDIR}/app/infiles.list
+echo "rc.cpp" >> ${WDIR}/app/infiles.list
+
+find ./notifier -name '*.cpp' -o -name '*.h' -o -name '*.c' -o -name '*.qml' | sort > ${WDIR}/applet/infiles.list
+
 cd ${WDIR}
 xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
   -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
   --msgid-bugs-address="${BUGADDR}" \
-  --files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
+  --files-from=app/infiles.list -D ${BASEDIR} -D ${WDIR}/app -o app/${PROJECT}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
+
+xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
+  -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
+  --msgid-bugs-address="${BUGADDR}" \
+  --files-from=applet/infiles.list -D ${BASEDIR} -D ${WDIR}/applet -o applet/${APPLET}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
 echo "Done extracting messages"
  
  
 echo "Merging translations"
-catalogs=`find . -name '*.po'`
+catalogs=`find ./app -name '*.po'`
 for cat in $catalogs; do
   echo $cat
   msgmerge -o $cat.new $cat ${PROJECT}.pot
   mv $cat.new $cat
 done
+catalogs=`find ./applet -name '*.po'`
+for cat in $catalogs; do
+  echo $cat
+  msgmerge -o $cat.new $cat ${APPLET}.pot
+  mv $cat.new $cat
+done
+
 echo "Done merging translations"
  
  
 echo "Cleaning up"
 cd ${WDIR}
-rm rcfiles.list
-rm infiles.list
-rm rc.cpp
+rm app/rcfiles.list
+rm app/infiles.list
+rm app/rc.cpp
+rm applet/infiles.list
 echo "Done"
